7ec74529f6929c6b902d08493f3bc8a5af68803c
Christie Reid <christiereid@protonmail.com>
2022-08-25 09:56:49 +0200

    document setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./scripts/*.{html,js,ts}", "./styles/*.{html,js,ts"],
+	content: ["./**/*.{html,js,ts}"],
 	theme: {
 		extend: {},
 	},
