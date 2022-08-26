a8622e6260c31cdc68534c84cff0a36d8a5e19ab
Christie Reid <christiereid@protonmail.com>
2022-08-26 14:22:27 +0200

    finish mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ["./src/**/*.{html,js,ts}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bg1: "#eceeee",
+				bg2: "#2d4055",
+				font: "#051320",
+				shape1: "#76dcc2",
+				shape2: "#051320",
+				shape3: "#6a7c86",
+			},
+		},
 	},
 	plugins: [],
 };
